4239c51f972011c2fa7afb9c21d7615a42492072
Tomi Hardem <tomihardem@gmail.com>
2026-06-10 00:18:25 -0300

    Primer commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-600">
+        🚚 Grúa 24HS Ludueña
+      </h1>
+    </main>
+  );
+}
